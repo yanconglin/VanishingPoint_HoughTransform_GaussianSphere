77090a9b3d9ef38d67bb0b0b0eaeb9fa2b2a96e5
CVisioner <yanconglin@users.noreply.github.com>
2022-03-28 09:25:29 +0200

    Update im2ht_cuda.cuh

--- vpd/models/ht/cpp_im2ht/im2ht_cuda.cuh
+++ vpd/models/ht/cpp_im2ht/im2ht_cuda.cuh
@@ -26,6 +26,7 @@ __global__ void im2ht_cuda_forward_kernel(const int n,
                                 )
 {
   // launch channel*num_votes   cores
+  // todo: coalescing
   CUDA_KERNEL_LOOP(index, n)
   {
   // input: [batch, channel, height, width]
@@ -79,6 +80,7 @@ __global__ void im2ht_cuda_backward_kernel(const int n,
                                   )
 {
   // launch channel * height *  ht_w  cores
+  // todo: coalescing
   CUDA_KERNEL_LOOP(index, n)
   {
     // input: [batch, channel, height, width]
